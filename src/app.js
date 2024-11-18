4380f23aebed5c9c082ff466bc836fb92e5ca328
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-11-18 01:52:31 +0000

    resultadoproyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronome = ["el", "nuestro", "su"];
+  let adjetivo = ["genial", "grande", "asombroso"];
+  let substantivo = ["corredor", "mapache", "perro", "mercador", "condutor"];
+
+  for (let i = 0; i < pronome.length; i++) {
+    for (let j = 0; j < adjetivo.length; j++) {
+      for (let k = 0; k < substantivo.length; k++) {
+        console.log(pronome[i] + adjetivo[j] + substantivo[k] + ".com");
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
